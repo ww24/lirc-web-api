a9210db05d84acc7fb9df932c3409a47a515367d
Takenori Nakagawa <ww24gm@gmail.com>
2016-12-29 00:29:42 +0900

    support systemctl

--- install.sh
+++ install.sh
@@ -10,11 +10,40 @@ case $ARCH in
 i386) ARCH="386";;
 esac
 OS=$(uname | tr '[:upper:]' '[:lower:]')
-if [ $OS = "windows" ]; then
+if [ "$OS" = "windows" ]; then
   EXT=".exe"
 fi
 VERSION=$(curl -sL https://raw.githubusercontent.com/ww24/lirc-web-api/master/wercker.yml | grep 'API_VERSION="v' | awk -F\" '{print $2}')
-curl -sLo api "https://github.com/ww24/lirc-web-api/releases/download/${VERSION}/api_${OS}_${ARCH}${EXT}"
-chmod +x api
+curl -Lo /tmp/lirc-web-api "https://github.com/ww24/lirc-web-api/releases/download/${VERSION}/api_${OS}_${ARCH}${EXT}"
+chmod +x /tmp/lirc-web-api
 
-echo "installed at $(pwd)/api $(./api -v)"
+if [ "$OS" = "linux" ] && hash systemctl; then
+  INSTALL_DIR=/usr/local/bin
+  mkdir -p $INSTALL_DIR
+  INSTALL_PATH="$INSTALL_DIR/lirc-web-api"
+  sudo mv /tmp/lirc-web-api $INSTALL_PATH
+
+  cat <<EOF | sudo tee /lib/systemd/system/lirc-web-api.service
+[Unit]
+Description=lirc-web-api
+After=network.target network-online.target
+
+[Service]
+Type=simple
+ExecStart=$INSTALL_PATH
+ExecReload=/bin/kill -HUP \$MAINPID
+KillMode=control-group
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+Alias=mackerel-agent.service
+EOF
+
+  sudo systemctl enable lirc-web-api
+  sudo systemctl start lirc-web-api
+  sudo systemctl status lirc-web-api
+else
+  mv /tmp/lirc-web-api .
+  echo "installed at $(pwd)/lirc-web-api $(./lirc-web-api -v)"
+fi
